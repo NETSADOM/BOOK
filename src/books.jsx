04f6a763087c5a64e754ebcff9ff65b537e03917
NETSADOM <netsanettesfaye122@gmail.com>
2025-02-26 09:51:20 +0300

    book display

---
+++ src/books.jsx
@@ -0,0 +1,4 @@
+export const books=[{id:1, title:'Atomic Habit',author:'James Clear', img:'https://m.media-amazon.com/images/I/419CqGgAdZL._SY445_SX342_.jpg'
+}
+,{id:2, title:'The Way Things Work', author:'David Macaulay', img:'https://m.media-amazon.com/images/I/518aWBvoe-L._SX342_SY445_.jpg'}]
+ 
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import "./styles.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import {books} from './books'
+  const BookList=()=>{
+    return(
+    <section className='Booklist'>
+     {books.map((book)=>{
+      return <Book key={book.id} {...book} ></Book>
+     })}
+     </section>
+    );
+  }
+  const Book=({img,title,author})=>{
+    const clickHandler=(e)=>{
+      console.log(e);
+      console.log(e.target)
+      alert('hello world');
+    };
+    return(
+      <article className='book'>
+         <img src={img}/> 
+          <h1>{title}</h1>
+        <h4>{author}</h4>
+        <button onClick={clickHandler}>click here</button>
+      </article>
+    );
+  }
+  
+ 
+  const root = document.getElementById('root');
 
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(root).render(<BookList  />);
