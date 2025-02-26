7479785c692782238f4539f929a3e4126105dc99
NETSADOM <netsanettesfaye122@gmail.com>
2025-02-26 10:25:58 +0300

    books

--- src/books.jsx
+++ src/books.jsx
@@ -1,4 +1,8 @@
 export const books=[{id:1, title:'Atomic Habit',author:'James Clear', img:'https://m.media-amazon.com/images/I/419CqGgAdZL._SY445_SX342_.jpg'
 }
-,{id:2, title:'The Way Things Work', author:'David Macaulay', img:'https://m.media-amazon.com/images/I/518aWBvoe-L._SX342_SY445_.jpg'}]
+,{id:2, title:'The Way Things Work', author:'David Macaulay', img:'https://m.media-amazon.com/images/I/518aWBvoe-L._SX342_SY445_.jpg'
+
+},{id:3, title:"THE LET THEM THEORY",  author:"Mel Robbins", img:"https://m.media-amazon.com/images/I/51wzfAWW1bL._SY445_SX342_.jpg"
+
+},{id:4, title:" The Nightingale: A Novel", author:"Kristin Hannah", img:"https://m.media-amazon.com/images/I/81OkWjcf4WL._SY466_.jpg"}]
  
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import {books} from './books'
+
   const BookList=()=>{
     return(
     <section className='Booklist'>
@@ -12,20 +13,28 @@ import {books} from './books'
     );
   }
   const Book=({img,title,author})=>{
+    const [hover, setHover] = React.useState(false);
     const clickHandler=(e)=>{
       console.log(e);
       console.log(e.target)
-      alert('hello world');
+      window.open("https://www.amazon.com/Best-Sellers-Books/zgbs/books", "_blank");
     };
-    return(
-      <article className='book'>
-         <img src={img}/> 
-          <h1>{title}</h1>
+  
+    return (
+      <div className='book' style={{ boxShadow: hover ? "5px 5px 15px rgba(0, 0, 0, 0.3)" : "3px 3px 10px rgba(0, 0, 0, 0.2)",
+        transform: hover ? "scale(1.05)" : "scale(1)",
+        transition: "all 0.3s ease",}} onMouseEnter={()=>setHover(true)} onMouseLeave={() => setHover(false)}>
+        <img src={img} alt={title} />
+        <h2>{title}</h2>
         <h4>{author}</h4>
-        <button onClick={clickHandler}>click here</button>
-      </article>
+        <button className='btn' style={{
+          boxShadow: hover ? "5px 5px 15px rgba(0, 0, 0, 0.3)" : "3px 3px 10px rgba(0, 0, 0, 0.2)",
+          transform: hover ? "scale(1.05)" : "scale(1)",
+          transition: "all 0.3s ease",
+        }} onClick={clickHandler} onMouseEnter={()=>setHover(true)} onMouseLeave={() => setHover(false)}>Buy on Amazon</button>
+      </div>
     );
-  }
+  };
   
  
   const root = document.getElementById('root');
